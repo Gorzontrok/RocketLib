6ada180e1c454da8297d41c70e844bc9702647c2
Gorzontrok <64686176+Gorzontrok@users.noreply.github.com>
2025-10-18 08:18:21 +0200

    Update AssemblyInfo.cs

--- RocketLib/Properties/AssemblyInfo.cs
+++ RocketLib/Properties/AssemblyInfo.cs
@@ -2,10 +2,10 @@
 using System.Runtime.InteropServices;
 using static RocketLib.Constants;
 
-[assembly: AssemblyTitle("RocketLibUMM")]
+[assembly: AssemblyTitle("RocketLib")]
 [assembly: AssemblyCompany(AUTHOR)]
-[assembly: AssemblyProduct("RocketLibUMM")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyProduct("RocketLib")]
+[assembly: AssemblyCopyright("Copyright © Gorzontrok 2025")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("a6375eb5-1a4c-48e5-b2bf-2ced7175edf4")]
