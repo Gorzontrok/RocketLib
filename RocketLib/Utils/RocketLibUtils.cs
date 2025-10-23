ec94968181c7e88bc798b42f87b2fc53a609316d
Alex Neargarder <alexneargarder@yahoo.com>
2025-10-22 23:18:54 -0400

    Add function for determining root directory

--- RocketLib/Utils/RocketLibUtils.cs
+++ RocketLib/Utils/RocketLibUtils.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using System.Reflection;
+using UnityEngine;
 
 namespace RocketLib.Utils
 {
@@ -15,6 +17,30 @@ namespace RocketLib.Utils
             {
                 gameObject.tag = "Unpausable";
             }
+        }
+
+        internal static string rootDirectoryPath = string.Empty;
+
+        public static string GetRootDirectory()
+        {
+            if (rootDirectoryPath != string.Empty)
+            {
+                return rootDirectoryPath;
+            }
+            string assemblyPath = Assembly.GetExecutingAssembly().Location;
+            DirectoryInfo dir = Directory.GetParent(assemblyPath);
+
+            // Find mods directory and use parent as root
+            if (dir.Parent.Name == "Mods")
+            {
+                rootDirectoryPath = dir.Parent.Parent.FullName;
+            }
+            else if (dir.Parent.Parent.Name == "Mods")
+            {
+                rootDirectoryPath = dir.Parent.Parent.Parent.FullName;
+            }
+
+            return rootDirectoryPath;
         }
     }
 }
